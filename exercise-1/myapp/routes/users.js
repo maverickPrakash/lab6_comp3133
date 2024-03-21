70fd6c2f3c876c55156485b3e376707d457f88c7
maverickPrakash <Prakashtheryon@gmail.com>
2024-03-21 14:46:11 -0400

    lab 7

--- exercise-1/myapp/routes/users.js
+++ exercise-1/myapp/routes/users.js
@@ -1,17 +1,18 @@
 var express = require('express');
-var router = express.Router();
 var bodyParser = require('body-parser')
-
+var router = express.Router();
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', function(req, res, next) {
-  console.log(`First Name: ${req.body.firstname}`)
-  console.log(`Last Name: ${req.body.lastname}`)
-  res.send('POST received!')
+router.post('/', function(req,res ){
+  console.log(req.body.fnm)
+  console.log(req.body.fnm)
+
+  res.send("Post Recieved")
 })
 
-router.use(bodyParser.urlencoded({extended: true}))
 module.exports = router;
